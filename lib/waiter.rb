dd57b4b70164504888d8f17619f0251423cec298
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 13:44:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -9,13 +9,17 @@ class Waiter
     @@all.push(self)
   end
   
-  def new_meal
+  def new_meal(customer, total, tip = 0)
+    Meal.new(self, customer, total, tip)
   end
   
   def meals
+      Meal.all.select do |meals|
+        meal.waiter == self
   end
   
   def best_tipper
+    
   end
  
   def self.all

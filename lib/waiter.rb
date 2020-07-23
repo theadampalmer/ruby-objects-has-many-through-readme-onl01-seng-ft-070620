c73a3611b835f4b38ecb6c210e26807f1da4165d
Adam Palmer <theadampalmer@gmail.com>
2020-07-23 13:37:13 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,25 @@
 class Waiter
-
+  attr_accessor :name, :years
+  
+  @@all = []
+  
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all.push(self)
+  end
+  
+  def new_meal
+  end
+  
+  def meals
+  end
+  
+  def best_tipper
+  end
+ 
+  def self.all
+    @@all
+  end
+  
 end
